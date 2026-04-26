67b0112e7aeb525109ba3d1aa6fbe76cbb6c0e46
wakan <wakana1889@gmail.com>
2026-04-26 15:58:45 +0900

    fix: remove dotenv

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,6 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_KEY!;
 
+console.log("DEBUG URL:", supabaseUrl);
+console.log("DEBUG KEY:", supabaseKey?.slice(0, 10));
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase env");
 }
